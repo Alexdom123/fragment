b2c5adcddda57bd30dd1375d74df106a0d49f4e3
Alexdom <alexmoreno.am455@gmail.com>
2019-12-08 20:26:16 -0600

    commit principal

---
+++ app/src/main/java/moreno/alex/fragmentos/test.kt
@@ -0,0 +1,14 @@
+package moreno.alex.fragmentos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class test : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+
+
+    }
+}
